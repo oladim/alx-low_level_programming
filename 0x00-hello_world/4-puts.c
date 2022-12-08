0f5d826f32a4e4c64eddb537a00c7a7fc07a2f4f
oladim <ogunrinuabideen@gmail.com>
2022-12-08 07:02:32 -0800

    betty lint

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * Program to print a string using puts
+ * main - Program to print a string using puts
+ *
+ * Return:  Always 0 Success
  */
 int main(void)
 {
+	/* the program store string value */
 char str1[] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
