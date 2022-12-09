b3a9e23a79e10f1784add83f893a1dd85b8d85d7
oladim <ogunrinuabideen@gmail.com>
2022-12-09 03:06:45 -0800

    ifthenelse

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry for the program
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%u is positive\n", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else
+	{
+		printf("%u is zero\n", n);
+	}
+	return (0);
+
+}
