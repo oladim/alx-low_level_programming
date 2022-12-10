43ee7fbedd50707c1e5ec6577b5917cb19b9d1ca
oladim <ogunrinuabideen@gmail.com>
2022-12-10 07:13:47 -0800

    print with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - welcome to the program
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
