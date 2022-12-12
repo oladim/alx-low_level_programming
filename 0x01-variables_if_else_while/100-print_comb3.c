f790a3f003a0dfb6dfebc90d2e8e0f7028f063b0
oladim <ogunrinuabideen@gmail.com>
2022-12-12 02:13:53 -0800

    combing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program to comb values
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i == 8)
+			{
+				putchar(' ');
+			} else 
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
