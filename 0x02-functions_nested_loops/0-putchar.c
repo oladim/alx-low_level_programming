e532e34b7f864196c1bc06c7e7282339d17e25e7
oladim <ogunrinuabideen@gmail.com>
2022-12-13 13:20:52 -0800

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "_putchar.c"
+
+/**
+ * main - program to start
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	char toprint[] = "_putchar";
+	while (i < (int) strlen(toprint) - 1)
+	{
+		_putchar(toprint[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
