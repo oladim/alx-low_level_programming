5cf3225564001ee06443116f08c8a6f06c40e5b5
oladim <ogunrinuabideen@gmail.com>
2022-12-15 02:27:48 -0800

    modify output

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,21 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d\n", i);
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d%c%c", i, 44, 32);
 		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d\n", i);
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d%c%c", i, 44, 32);
 		}
 	}
+	printf("\n");
 }
