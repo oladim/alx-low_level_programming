3cf83925b0cd6273f0192ecdf5dd50fd4abc844e
oladim <ogunrinuabideen@gmail.com>
2022-12-14 19:30:01 -0800

    print last digit of a value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - a function to print last digit of a value
+ *
+ * @v: parameter to get its last value
+ *
+ * Return: absolute value of the reminder
+ */
+
+int print_last_digit(int v)
+{
+	int mod = abs(v % 10);
+
+	return (mod);
+}
