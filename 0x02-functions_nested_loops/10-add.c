3f714a5950ba21390c1df15400bdf52e2e3cb0f0
oladim <ogunrinuabideen@gmail.com>
2022-12-15 01:50:19 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - sum of two numbers
+ *
+ * Return: sum of two numbers
+ *
+ * @a: paramer 2
+ * @b: parameter 1
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int r;
+
+	r = add(5, 6);
+	printf("%d\n", r);
+	return (0);
+}
