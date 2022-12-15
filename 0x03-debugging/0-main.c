2cd794af6b66c62bf118c2ad03eb4983b14e1ce9
oladim <ogunrinuabideen@gmail.com>
2022-12-15 03:27:30 -0800

    modify

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Return: Always 0 Success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
@@ -12,5 +11,6 @@ int main(void)
 
 	i = 98;
 	positive_or_negative(i);
+
 	return (0);
 }
