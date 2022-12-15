4c58aa2b4d8ad157540ebd69930c47bb91f17b3d
oladim <ogunrinuabideen@gmail.com>
2022-12-14 19:12:57 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ *_abs - gets the absolute value of an integer
+ *
+ * @j: parameter value to find its absolute value
+ *
+ * Return: Absolute value
+ */
+
+int _abs(int j)
+{
+	return (abs(j));
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
