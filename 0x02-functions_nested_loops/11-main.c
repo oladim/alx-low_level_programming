46afe80097230c7fda9d2db46d5f104a490bda52
oladim <ogunrinuabideen@gmail.com>
2022-12-15 02:15:00 -0800

    print ascending and descending

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print ascending or decending order
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	print_to_98(-1);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - printing ascending or descending
+ *
+ * @n: parameter
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d\n", i);
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d\n", i);
+		}
+	}
+}
