b89fb65117f7185feb5f7a02610971bfada3122f
oladim <ogunrinuabideen@gmail.com>
2022-12-15 03:11:35 -0800

    debug int

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry for the program
+ *
+ * Return: Always 0 Success
+ */
+
+int main(int y)
+
+{
+	if (y > 0)
+	{
+		printf("%u is positive\n", y);
+	} else if (y < 0)
+	{
+		printf("%d is negative\n", y);
+	} else
+	{
+		printf("%u is zero\n", y);
+	}
+	return (0);
+
+}
