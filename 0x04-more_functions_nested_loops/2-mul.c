73050a68c95ecb3883df1c08556eefffd494c913
oladim <ogunrinuabideen@gmail.com>
2022-12-16 11:49:53 -0800

    multiplcation

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply 2 numbers
+ * Return: value of multiplication
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
