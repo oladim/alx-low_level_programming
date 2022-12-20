3846e5b43c7918da33a2f855d2eb7322151adfcf
oladim <ogunrinuabideen@gmail.com>
2022-12-20 08:34:27 -0800

    print string value to stdout

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - to print string value
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
