c153f5d9b9c031a171dc32c2d7409486c1354ccb
oladim <ogunrinuabideen@gmail.com>
2022-12-21 08:10:28 -0800

    reverse string via memory address

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - print string value
+ * @s: parameter to print
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int len;
+	int dec;
+
+	char scop[10];
+
+	strcpy(scop, s);
+	len = strlen(s) - 1;
+	dec = strlen(s) - 1;
+
+	while (i <= dec)
+	{
+		s[i] = scop[len];
+		i++;
+		len--;
+	}
+}
