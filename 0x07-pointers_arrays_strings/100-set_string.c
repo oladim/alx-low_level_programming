ee5634a071bc547ee8fa721368c0e9d2e0183fc7
oladim <ogunrinuabideen@gmail.com>
2023-01-03 05:34:32 -0800

    changing memory value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - function to change value of an address
+ * @s: changing from
+ * @to: to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
