2b48f5345f26187d5cb4929cad1cf7963a53caed
oladim <ogunrinuabideen@gmail.com>
2023-01-04 23:20:35 -0800

    puts in recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function to print string using recursion
+ * @s: param
+ */
+
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
