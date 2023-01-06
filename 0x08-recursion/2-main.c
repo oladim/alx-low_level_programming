cb4b31b0c19d3325b423569fa24c7f063482eada
oladim <ogunrinuabideen@gmail.com>
2023-01-05 23:37:08 -0800

    length recursion

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - function using recursion to print
+ * @s: param
+ * Return: length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
