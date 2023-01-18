0b81fe976c7a855537411e445968ff1473f7b553
oladim <ogunrinuabideen@gmail.com>
2023-01-18 04:52:34 -0800

    argsv printing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments from the prompt
+ * @argv: array of arguments
+ * Return: Always 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
